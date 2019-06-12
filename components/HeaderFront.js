ffb97de6ae700ebad2a86227d1f110442d5a593c
patrykmaron <up705461@myport.ac.uk>
2019-06-12 22:05:44 +0100

    svgs imported

--- components/HeaderFront.js
+++ components/HeaderFront.js
@@ -3,6 +3,11 @@
 */
 import Link from 'next/link'
 import { useState } from 'react';
+import AzureSVG from '../static/azure.svg';
+import AzureLight from '../static/azure_light.svg';
+import Logo from '../static/logo.svg';
+import BigBlob from '../static/big_blob.svg';
+import BigBlobLight from '../static/big_blob_light.svg';
 
 
 function HeaderFront(){
@@ -11,13 +16,26 @@ function HeaderFront(){
 
     return(
         <header className="hero is-fullheight "  >
+            <div className="svg-container">
+            
+            <BigBlob width="35%" height="35%" className="big-blob" />
+            <BigBlobLight width="38%" height="38%" className="big-blob-light" />
+
+            <AzureSVG width="80%" height="80%" className="blue-azure" />
+            <AzureLight  width="83%" height="83%" className="blue-azure-light" />
+            
+
+
+            </div>  
             <nav className="navbar" role="navigation" aria-label="main navigation" style={{position: "absolute", width: "100%"}}>
                 <div className="container">
                     <div className="navbar-brand">
                         <Link href="/">
-                            <a className="navbar-item">
-                                <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+                            <a style={{height: 115, width: 75}}>
+                            <Logo width="100%" height="100%" />
                             </a>
+                               
+                            
                         </Link>
 
                         <span className="hamburger-custom" data-target="navMenu">
@@ -39,21 +57,23 @@ function HeaderFront(){
                     </div>
                 </div>
             </nav>
-
-            <div className="columns">
+        <div className="container">
+        <div className="columns">
             
-                <div className="column is-three-fifths" style={{padding: '4rem'}} >
-                    <h1 className="main-title">
-                       London's Most Exclusive Freelance Web Design
-                    </h1>
-                    <Link href="/contact">
-                        <a className="button is-medium is-rounded button-wider">Contact Me!</a>
-                    </Link>
-                </div>
-                <div className="column">
-                    Right
-                </div>
+            <div className="column is-6" style={{marginTop: "7rem"}} >
+                <h1 className="main-title">
+                   London's Most Exclusive Freelance Web Design
+                </h1>
+                <Link href="/contact">
+                    <a className="button is-medium is-rounded button-wider">Contact Me!</a>
+                </Link>
             </div>
+            <div className="column">
+                Right
+            </div>
+        </div>
+        </div>
+            
             
         </header>
     )
--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 
 // Ability to import SASS
 const withSass = require('@zeit/next-sass')
+
 module.exports = withSass({
-    /* config options */
-})
+    // SVG
+    webpack(config) {
+      config.module.rules.push({
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+      });
+  
+      return config;
+    }})
